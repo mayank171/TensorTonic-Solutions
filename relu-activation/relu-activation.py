04c210e90fcc9ef69e1cc94b599c6a111d4c503b
mayank <46345959+mayank171@users.noreply.github.com>
2026-02-08 18:35:29 +0530

    Update solution for relu-activation

--- relu-activation/relu-activation.py
+++ relu-activation/relu-activation.py
@@ -7,5 +7,7 @@ def relu(x):
     # Write code here
     x=np.asarray(x,dtype=float)
     y=np.maximum(0,x)
-    return y if y.ndim != 0 else y.reshape(1)
+    if y.ndim==0:
+        return y.reshape(1)
+    return y
     pass
